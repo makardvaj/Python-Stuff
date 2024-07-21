7ca8813a96e877f8fb44100f591c633ee3751def
makardvaj <141413540+makardvaj@users.noreply.github.com>
2024-07-21 21:52:56 +0530

    Create BasicestTable.sql

---
+++ Short Progs/BasicestTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Students(
+	Sr_No VARCHAR(6),
+	Name VARCHAR(100),
+	Roll_No VARCHAR(12),
+	Standard VARCHAR(2),
+	Sec VARCHAR(1),
+	Attendance INTEGER(3),
+	Marks DECIMAL(5, 2),
+	Grade CHAR(1)) ;
+
+ALTER TABLE Students
